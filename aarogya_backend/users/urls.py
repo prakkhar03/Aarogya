c9312df0954d99e9c0ff69aebc036ceed6842b95
prakkhar03 <prakkhar.n03@gmail.com>
2026-05-02 12:52:46 +0530

    Added email

--- aarogya_backend/users/urls.py
+++ aarogya_backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, MeView
+from .views import RegisterView, LoginView, MeView,UpdateEmailView
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
